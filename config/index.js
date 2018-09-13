ed5c6ccd2209e47b44e156bfb2063d56b98532c5
Yuhan zhong <715181149@qq.com>
2018-09-13 19:56:25 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -51,7 +51,7 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
+    assetsSubDirectory: 'static',   //此处应该多考虑文件层级
     assetsPublicPath: '../',
 
     /**
